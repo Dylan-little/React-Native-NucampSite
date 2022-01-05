0aea78fdaa5029913fecc5aa7dc1670e57abd348
Dylan Little <littledill25@gmail.com>
2022-01-04 19:38:42 -0500

    Activity Indicator

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -18,24 +18,47 @@ class About extends Component {
 
     render() {
         const renderPartner = ({item}) => {
-            return(
-                <ListItem 
+            return (
+                <ListItem
                     title={item.name}
                     subtitle={item.description}
                     leftAvatar={{source: {uri: baseUrl + item.image}}}
                 />
-            )
+            );
+        };
+        if (this.props.partners.isLoading) {
+            return (
+                <ScrollView>
+                    <Mission />
+                    <Card
+                        title='Community Partners'>
+                        <Loading />
+                    </Card>
+                </ScrollView>
+            );
+        }
+        if (this.props.partners.errMess) {
+            return (
+                <ScrollView>
+                    <Mission />
+                    <Card
+                        title='Community Partners'>
+                        <Text>{this.props.partners.errMess}</Text>
+                    </Card>
+                </ScrollView>
+            );
         }
         return(
             <ScrollView>
-                    <Mission />
-                    <Card title="Community Partners">
-                        <FlatList 
-                            data={this.props.partners.partners}
-                            renderItem={renderPartner}
-                            keyExtractor={item => item.id.toString()}
-                        />
-                    </Card>
+                <Mission />
+                <Card 
+                    title="Community Partners">
+                    <FlatList 
+                        data={this.props.partners.partners}
+                        renderItem={renderPartner}
+                        keyExtractor={item=>item.id.toString()}
+                    />
+                </Card>
             </ScrollView>
         )
     }
--- components/DirectoryComponent.js
+++ components/DirectoryComponent.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
-import { FlatList } from 'react-native';
-import { ListItem } from 'react-native-elements';
+import Loading from './LoadingComponent';
+import { View, FlatList, Text } from 'react-native';
 import { Tile } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
@@ -31,6 +31,16 @@ class Directory extends Component {
             );
         };
 
+        if (this.props.campsites.isLoading) {
+            return <Loading />;
+        }
+        if (this.props.campsites.errMess) {
+            return (
+                <View>
+                    <Text>{this.props.campsites.errMess}</Text>
+                </View>
+            );
+        }
         return (
             <FlatList
                 data={this.props.campsites.campsites}
